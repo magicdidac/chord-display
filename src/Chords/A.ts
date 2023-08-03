a9619e09a807b588e4263f1756ef9a0031dcaf98
dpadilla <didacpadilla20@gmail.com>
2023-08-03 17:58:21 +0200

    Added 2 more chords

--- src/Chords/A.ts
+++ src/Chords/A.ts
@@ -10,6 +10,18 @@ export const Am: Chord = {
   frets: [-1, 0, 2, 2, 1, 0]
 }
 
+export const Asharp: Chord = {
+  name: 'A#',
+  barre: 1,
+  frets: [-1, 0, 2, 2, 2, 0]
+}
+
+export const Asharpm: Chord = {
+  name: 'A#m',
+  barre: 1,
+  frets: [-1, 0, 2, 2, 1, 0]
+}
+
 export const Am9: Chord = {
   name: 'Am9',
   barre: 5,
